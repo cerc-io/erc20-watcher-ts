889eae79188eedf2fd0baea5f8b3c2f31f12db99
prathamesh0 <42446521+prathamesh0@users.noreply.github.com>
2023-10-18 17:15:06 +0530

    Take account key from env and add a task to check balance (#7)
    * Take account key from env and add a task to check balance
    
    * Fix task description
    
    * Use dotenv in hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,8 @@
 // Copyright 2021 Vulcanize, Inc.
 //
 
+import 'dotenv/config';
+
 import '@nomiclabs/hardhat-waffle';
 
 import './test/tasks/token-deploy';
@@ -10,9 +12,12 @@ import './test/tasks/token-approve';
 import './test/tasks/token-transfer-from';
 import './test/tasks/block-latest';
 import './test/tasks/account';
+import './test/tasks/token-balance';
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
+
+const accounts = process.env.ACCOUNT_PRIVATE_KEY ? [process.env.ACCOUNT_PRIVATE_KEY] : undefined;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -21,7 +26,8 @@ export default {
   solidity: '0.8.0',
   networks: {
     docker: {
-      url: process.env.ETH_RPC_URL || 'http://go-ethereum:8545'
+      url: process.env.ETH_RPC_URL || 'http://go-ethereum:8545',
+      accounts
     }
   },
   paths: {
---
+++ test/tasks/token-balance.ts
@@ -0,0 +1,19 @@
+//
+// Copyright 2023 Vulcanize, Inc.
+//
+
+import { task, types } from 'hardhat/config';
+import '@nomiclabs/hardhat-ethers';
+
+task('token-balance', 'Check token balance for an account')
+  .addParam('token', 'Token contract address', undefined, types.string)
+  .addParam('account', 'Account address', undefined, types.string)
+  .setAction(async (args, hre) => {
+    const { token: tokenAddress, account } = args;
+    await hre.run('compile');
+    const Token = await hre.ethers.getContractFactory('GLDToken');
+    const token = Token.attach(tokenAddress);
+
+    const result = await token.balanceOf(account);
+    console.log('Result', result);
+  });
